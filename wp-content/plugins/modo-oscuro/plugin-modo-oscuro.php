28312a5bbfd08018d4e692025db010bdd585c8d9
Roso Peñaranda <rosopenaranda@gmail.com>
2020-08-24 09:19:30 -0500

    finalizado curso

---
+++ wp-content/plugins/modo-oscuro/plugin-modo-oscuro.php
@@ -0,0 +1,17 @@
+<?php
+
+//Plugin name: Modo Oscuro
+//Description: Activa el modo oscuro en tu theme
+//version: 1.0
+//Author: Roso Peñaranda
+//Author URI: https://gitgub.com/rosoPenaranda
+
+function estilos_plugin(){
+
+  $estilos_url = plugin_dir_url(__FILE__);
+
+  wp_enqueue_style( 'modo_oscuro', $estilos_url.'/assets/css/estilos.css', '', '1.0', 'all' );
+
+}
+
+add_action( 'wp_enqueue_scripts', 'estilos_plugin' );
---
+++ wp-content/themes/platzigifts/404.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+<main class="container my-5">
+  <div class="pagina404">
+    <h1>404 PÁGINA NO ENCONTRADA</h1>
+    <h2>Haga click <a href="<?php echo home_url()?>">aquí</a> para volver a la página principal</h2>
+  </div>
+</main>
+<?php get_footer();
